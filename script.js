e5c527c8c744882f8b3d60a6ae1843412756e0e1
Pooja Mamgain <60378630+poojamgn@users.noreply.github.com>
2022-04-23 21:59:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+a = new Date();
+htime = a.getHours();
+mtime = a.getMinutes();
+stime = a.getSeconds();
+
+hrotation = 30*htime + mtime/2 ;
+mrotation = 6*mtime;
+srotation = 6*stime;
+
+hour.style.transform = `rotate(${hrotation}deg)`; 
+minute.style.transform = `rotate(${mrotation}deg)`; 
+second.style.transform = `rotate(${srotation}deg)`; 
+}, 1000); 
